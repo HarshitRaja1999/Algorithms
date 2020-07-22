81d50b9743e2138a17979f891a069dc780e8541c
HarshitRaja1999 <rajanovember199@gmail.com>
2020-07-22 11:57:20 +0530

    abs First Commit

---
+++ Maths/abs_value.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Absolute_value
+{
+    static void abs_value(double number)
+    {
+        if(number < 0)
+        {
+            Console.WriteLine("The Answer is :\t" + -number);
+        }
+        else
+        {
+            Console.WriteLine("The Answer is :\t" + number);
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter the Value : \t");
+
+        var a = Convert.ToDouble(Console.ReadLine());
+
+        abs_value(a);
+    }
+}
