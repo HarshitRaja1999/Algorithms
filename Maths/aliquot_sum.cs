e83d92141486dfb3180738c884f5b7c234977c01
HarshitRaja1999 <rajanovember199@gmail.com>
2020-07-22 12:40:26 +0530

    Maths Methods

---
+++ Maths/aliquot_sum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Aliquot_Sum
+{
+    void aliquot_sum(int num)
+    {
+        List<int> dividers = new List<int>();
+        for (int i = 1; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                dividers.Add(i);
+            }
+        }
+
+        double total = dividers.Sum();
+
+        Console.WriteLine("The Total is :\t" + total);
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Aliquot_Sum :");
+        Console.WriteLine("Enter the Number :");
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        aliquot_sum(number);
+    }
+}
+
---
+++ Maths/main.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maths
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Aliquot_Sum testing
+            // Aliquot_Sum ors =  new Aliquot_Sum();
+            // ors.Run();
+
+            // Aliquot_Sum testing
+            Absolute_value ors =  new Absolute_value();
+            ors.Run();
+        }
+    }
+}
